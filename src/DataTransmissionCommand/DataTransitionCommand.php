5c8f02ec963b87ccc3ed0faeeddd82961053b145
Mario Škrlec <whitepostmail@gmail.com>
2017-10-01 17:19:06 +0200

    Finished DataTransitionCommand

--- src/DataTransmissionCommand/DataTransitionCommand.php
+++ src/DataTransmissionCommand/DataTransitionCommand.php
@@ -2,7 +2,9 @@
 
 namespace DataTransmissionCommand;
 
-class DataTransitionCommand implements \Countable, \ArrayAccess
+use Assert\Assertion;
+
+class DataTransitionCommand implements \Countable, \ArrayAccess, \IteratorAggregate
 {
     /**
      * @var \RecursiveIteratorIterator $recursiveDataIterator
@@ -13,41 +15,137 @@ class DataTransitionCommand implements \Countable, \ArrayAccess
      */
     private $tokens = [];
     /**
+     * @var array $syntaxErrorBuild
+     */
+    private $syntaxErrorBuild = [];
+    /**
+     * @var array $result
+     */
+    private $result = [];
+    /**
      * DataTransitionCommand constructor.
      * @param array $transitions
      * @param array $data
      */
     public function __construct(array $transitions, array $data)
     {
-        $this->recursiveDataIterator = new \RecursiveIteratorIterator(new \RecursiveArrayIterator($data));
+        if (empty($transitions)) {
+            throw new \RuntimeException('DataTransitionCommand: No transitions. You passed an empty value');
+        }
+
+        if (empty($data)) {
+            throw new \RuntimeException('DataTransitionCommand: No data. You passed an empty value');
+        }
+
+        $this->recursiveDataIterator = new \RecursiveIteratorIterator(
+            new \RecursiveArrayIterator($data),
+            \RecursiveIteratorIterator::SELF_FIRST
+        );
 
         $this->parseTransitions($transitions);
-    }
+        $this->createTransitionObject();
 
-    public function count()
+        if (!empty($this->syntaxErrorBuild)) {
+            $this->handleSyntaxErrors();
+        }
+    }
+    /**
+     * @param $name
+     * @return mixed
+     */
+    public function __get($name)
     {
-    }
+        if (!array_key_exists($name, $this->result)) {
+            throw new \RuntimeException(
+                sprintf('Property \'%s\' not found', $name)
+            );
+        }
 
-    public function offsetExists($offset)
+        return $this->result[$name];
+    }
+    /**
+     * @void
+     */
+    private function createTransitionObject()
     {
-    }
+        $result = $this->buildFoundTree();
 
-    public function offsetSet($offset, $value)
+        if (empty($result)) {
+            throw new \RuntimeException(
+                sprintf('DataTransitionCommand: Something went wrong. Results are empty and not parsed. This is an internal error')
+            );
+        }
+
+        $this->result = $result;
+    }
+    /**
+     * @return array
+     */
+    private function buildFoundTree()
     {
-    }
+        $searchingFor = 0;
+        $found = [];
+        $previousDepth = null;
+        $inSearchMode = false;
+        foreach ($this->tokens as $token) {
+            foreach ($this->recursiveDataIterator as $key => $value) {
+                $tree = $token['tree'];
+                $currentDepth = $this->recursiveDataIterator->getDepth();
 
-    public function offsetGet($offset)
-    {
-    }
+                if (is_null($previousDepth)) {
+                    $previousDepth = $currentDepth;
+                }
 
-    public function offsetUnset($offset)
-    {
-    }
+                if ($inSearchMode === true) {
+                    if ($previousDepth !== ($currentDepth - 1)) {
+                        continue;
+                    }
+                }
 
+                if (
+                    $key === $tree[$searchingFor] and
+                    $currentDepth === $searchingFor
+                ) {
+                    if ((count($tree) - 1) === $currentDepth) {
+                        $this->assert($value, $token);
+
+
+                        if (is_string($token['alias'])) {
+                            $found[$token['alias']] = $value;
+                        } else {
+                            $found[$tree[count($tree) - 1]] = $value;
+                        }
+
+                        $previousDepth = null;
+                        $searchingFor = 0;
+                        $inSearchMode = false;
+
+                        break;
+                    } else {
+                        $previousDepth = $currentDepth;
+                        ++$searchingFor;
+                        $inSearchMode = true;
+                    }
+                }
+            }
+        }
+
+        return $found;
+    }
+    /**
+     * @param array $transitions
+     * @void
+     */
     private function parseTransitions(array $transitions)
     {
         foreach ($transitions as $transition) {
-            $this->tokens[] = $this->parseSingleTransition($transition);
+            $token = $this->parseSingleTransition($transition);
+
+            if (is_null($token)) {
+                continue;
+            }
+
+            $this->tokens[] = $token;
         }
     }
     /**
@@ -58,7 +156,162 @@ class DataTransitionCommand implements \Countable, \ArrayAccess
     {
         $this->validateTransition($transition);
 
+        $parsedToken = $this->parseToken($transition);
 
+        $this->tokens[] = $parsedToken;
+    }
+    /**
+     * @param string $transition
+     * @return array
+     */
+    private function parseToken($transition)
+    {
+        $parsedToken = [];
+
+        $mainParsedData = $this->parseTree($transition);
+
+        $parsedToken['tree'] = $mainParsedData['tree'];
+        $parsedToken['searchItem'] = $mainParsedData['searchItem'];
+        $parsedToken['assertions'] = $this->parseAssertions($transition);
+        $parsedToken['alias'] = $this->parseAlias($transition);
+
+        return $parsedToken;
+    }
+    /**
+     * @param string $transition
+     * @return array
+     */
+    private function parseTree($transition)
+    {
+        $parsedToken = [];
+        $parseTree = null;
+        $unparsed = null;
+
+        if (preg_match('#>#', $transition) === 1) {
+            $unparsed = preg_split('#>#', $transition);
+
+            $tree = [];
+            foreach ($unparsed as $index => $item) {
+                if (preg_match('#\##', $item) === 0 and preg_match('#\|#', $item) === 0) {
+                    $tree[] = $item;
+                }
+            }
+
+            $parseTree = $tree;
+
+            $last = $unparsed[count($unparsed) - 1];
+
+            if (preg_match('#\w+\##', $last)) {
+                $searchItem = preg_split('#\##', $last)[0];
+            } else {
+                $searchItem = $tree[count($tree) - 1];
+            }
+
+            $parseTree[] = $searchItem;
+
+            $parsedToken = [
+                'tree' => $parseTree,
+                'searchItem' => $searchItem,
+            ];
+        } else if (preg_match('#\##', $transition) === 1) {
+            $unparsed = preg_split('#\##', $transition);
+
+            $tree = [$unparsed[0]];
+            $searchItem = $unparsed[0];
+
+            unset($unparsed[0]);
+
+            $parsedToken = [
+                'tree' => $tree,
+                'searchItem' => $searchItem
+            ];
+        } else if (preg_match('#\|#', $transition) === 1) {
+            $pipeSplit = preg_split('#\|#', $transition);
+
+            $parsedToken = [
+                'tree' => [$pipeSplit[0]],
+                'searchItem' => $pipeSplit[0]
+            ];
+        }
+
+        return $parsedToken;
+    }
+    /**
+     * @param string $transition
+     * @return null|string
+     */
+    private function parseAlias($transition)
+    {
+        if (preg_match('#\|#', $transition) === 1) {
+            $alias = preg_split('#\|#', $transition);
+
+            if (count($alias) > 2) {
+                $this->syntaxErrorBuild[] = sprintf('DataTransitionCommand invalid transition. A transition can only have one alias. More that one given for transition %s', $transition);
+
+                $alias = null;
+            }
+
+            return $alias[1];
+        }
+
+        return null;
+    }
+    /**
+     * @param string $transition
+     * @return array
+     */
+    private function parseAssertions($transition)
+    {
+        if (preg_match('#\##', $transition) === 1) {
+            $assertions = preg_split('#\##', $transition);
+
+            // remove part that does not concern assertions, the rest of the array does
+            unset($assertions[0]);
+            $assertions = array_values($assertions);
+
+            $lastIndex = (count($assertions) > 1) ? (count($assertions) - 1) : 0;
+
+            if (preg_match('#\|#', $assertions[$lastIndex]) === 1) {
+                $pipedEntry = preg_split('#\|#', $assertions[$lastIndex]);
+
+                unset($assertions[$lastIndex]);
+
+                $assertions[] = $pipedEntry[0];
+            }
+            sort($assertions);
+
+            return $assertions;
+        }
+
+        return [];
+    }
+    /**
+     * @param mixed $found
+     * @param array $token
+     */
+    private function assert($found, array $token)
+    {
+        $assertions = $token['assertions'];
+
+        foreach ($assertions as $assertion) {
+            Assertion::{$assertion}(
+                $found,
+                sprintf('Failed assertion \'%s\' for search item \'%s\'', $assertion, $token['searchItem'])
+            );
+        }
+    }
+    /**
+     * @throws \InvalidArgumentException
+     */
+    private function handleSyntaxErrors()
+    {
+        $mainMessage = "Following syntax errors were encountered:\n";
+
+        foreach ($this->syntaxErrorBuild as $syntaxError) {
+            $mainMessage.="\t".$syntaxError."\n";
+        }
+
+        throw new \InvalidArgumentException($mainMessage);
     }
     /**
      * @param string $transition
@@ -78,4 +331,70 @@ class DataTransitionCommand implements \Countable, \ArrayAccess
             throw new \InvalidArgumentException($message);
         }
     }
+    /**
+     * @return int
+     */
+    public function count()
+    {
+        return count($this->result);
+    }
+    /**
+     * @param int|string $offset
+     * @return bool
+     */
+    public function offsetExists($offset)
+    {
+        return array_key_exists($offset, $this->result);
+    }
+
+    /**
+     * @param int|string $offset
+     * @param mixed $value
+     */
+    public function offsetSet($offset, $value)
+    {
+        if (!$this->offsetExists($offset)) {
+            throw new \OutOfBoundsException(sprintf('Property \'%s\' does not exist', $offset));
+        }
+
+        $this->result[$offset] = $value;
+    }
+    /**
+     * @param int|string $offset
+     * @return mixed
+     */
+    public function offsetGet($offset)
+    {
+        if ($this->offsetExists($offset)) {
+            return $this->result[$offset];
+        }
+
+        throw new \OutOfBoundsException(sprintf('Property \'%s\' does not exist', $offset));
+    }
+    /**
+     * @param int|string $offset
+     */
+    public function offsetUnset($offset)
+    {
+        if ($this->offsetExists($offset)) {
+            unset($this->result[$offset]);
+        }
+    }
+    /**
+     * @return \ArrayIterator
+     */
+    public function getIterator()
+    {
+        return new \ArrayIterator($this->result);
+    }
+    /**
+     * @return \Generator
+     */
+    public function getGenerator()
+    {
+        foreach ($this->result as $key => $item) {
+            yield $item;
+        }
+    }
+
 }
--- tests/Test/DataTransitionCommandTest.php
+++ tests/Test/DataTransitionCommandTest.php
@@ -33,10 +33,27 @@ class DataTransitionCommandTest extends \PHPUnit_Framework_TestCase
                 'id' => 5,
                 'version' => 5,
             ],
-            'platformAccountId' => 5,
+            'platformAccountId' => 6,
             'productAccountId' => 5,
             'platformLinkUuid' => Uuid::uuid4()->toString()
         ];
+    }
+
+    public function test_invalid_alias_syntax()
+    {
+        $didThrowException = false;
+
+        try {
+            new DataTransitionCommand([
+                'platformLinkUuid#string#uuid|linkUuid|multiple_alias'
+            ], $this->transition);
+        } catch (\InvalidArgumentException $e) {
+            $didThrowException = true;
+
+            var_dump($e->getMessage());
+        }
+
+        $this->assertTrue($didThrowException);
     }
     /**
      * @expectedException \InvalidArgumentException
@@ -47,5 +64,59 @@ class DataTransitionCommandTest extends \PHPUnit_Framework_TestCase
             1,
             6
         ], $this->transition);
+    }
+
+    public function test_transition()
+    {
+        $uuid = Uuid::uuid4()->toString();
+
+        $this->transition['platformLinkUuid'] = $uuid;
+
+        $command = new DataTransitionCommand([
+            'platformAccountId#integer',
+            'productAccountId|productAccountId',
+            'platformLinkUuid#uuid#string|linkUuid',
+            'productAccount>productVersion>product>id#integer|productId',
+            'productAccount>productVersion>product>category#string|productCategory',
+            'productAccount#isArray|productAccountArray'
+        ], $this->transition);
+
+        $this->assertEquals($command->platformAccountId, 6);
+        $this->assertEquals($command->productAccountId, 5);
+        $this->assertInternalType('array', $command->productAccountArray);
+        $this->assertEquals($command->linkUuid, $uuid);
+        $this->assertEquals($command->productId, 5);
+        $this->assertEquals($command->productCategory, 'Product category');
+    }
+
+    public function test_helper_methods()
+    {
+        $uuid = Uuid::uuid4()->toString();
+
+        $this->transition['platformLinkUuid'] = $uuid;
+
+        $command = new DataTransitionCommand([
+            'platformAccountId#integer',
+            'productAccountId|productAccountId',
+            'platformLinkUuid#uuid#string|linkUuid',
+            'productAccount>productVersion>product>id#integer|productId',
+            'productAccount>productVersion>product>category#string|productCategory',
+            'productAccount#isArray|productAccountArray'
+        ], $this->transition);
+
+        $this->assertEquals(6, count($command));
+        $this->assertEquals($command['platformAccountId'], 6);
+        $this->assertEquals($command['productAccountId'], 5);
+        $this->assertInternalType('array', $command['productAccountArray']);
+        $this->assertEquals($command['linkUuid'], $uuid);
+        $this->assertEquals($command['productId'], 5);
+        $this->assertEquals($command['productCategory'], 'Product category');
+
+        $this->assertInstanceOf(\Generator::class, $command->getGenerator());
+        $this->assertInstanceOf(\ArrayIterator::class, $command->getIterator());
+
+        foreach ($command as $key => $item) {
+            $this->assertEquals($item, $command->{$key});
+        }
     }
 }
